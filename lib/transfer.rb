65970832c882d3c38cc7b215e3cd776f121e2ccd
Iskra B <ibatistapoblete@gmail.com>
2019-05-25 08:20:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,7 @@ class Transfer
 
   def execute_transaction
     @sender.balance = @sender.balance - self.amount
+    @receiver.balance = @receiver.balance + self.amount
   
   end
   # your code here

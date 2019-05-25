f7bace3672c3bdcdb7871c1466b6ed88353c9227
Iskra B <ibatistapoblete@gmail.com>
2019-05-25 08:57:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   def execute_transaction
     if self.status == "pending" && @sender.valid? == true
       @sender.balance = @sender.balance - self.amount
-      @receiver.balance = @receiver.balance + self.amount
+      @receiver.balance.deposit(amount)
       self.status = "complete"
     else
       self.status = "rejected"
